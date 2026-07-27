ae4147e363ba6208ca62b9c771707d4b81c6563f
Yash Gupta <yashgupta8910890643@gmail.com>
2026-07-27 20:39:17 +0530

    Initial React projects

---
+++ 01reactbasic/src/App.js
@@ -0,0 +1,12 @@
+
+import Chai from "./Chai";
+function App() {
+  return (
+    <div>
+  <h1>yash gupta react</h1>
+  <Chai/>
+  </div>
+  );
+}
+
+export default App;
---
+++ 01reactbasic/src/Chai.js
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+       <h1>chai with react is ready</h1>
+    )
+}
+export default Chai;
+// functions name should be in capatilize way means 1st letter capital hoga 
